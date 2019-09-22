71d91f545901ef33f0689ad60e443018b0d80c19
Juan <j.juancamilojurado@gmail.com>
2019-09-22 11:47:06 -0500

    Creacion del programa

---
+++ Convertidor/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convertidor
+{
+    public partial class Form1 : Form
+    {
+        Temperatura temperatura = new Temperatura();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            temperatura.setGrados(double.Parse(txtgradosc.Text));
+
+            lbltotal.Text = "Grados Fahrenheit: " + temperatura.convertir().ToString();
+            lbltotal.Visible = true;
+        }
+    }
+}
---
+++ Convertidor/Temperatura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertidor
+{
+    public class Temperatura
+    {
+        private double centigrados;
+
+        public void setGrados(double valor)
+        {
+            this.centigrados = valor;
+
+        }
+
+        public double getCentigrados()
+        {
+            return this.centigrados;
+        }
+
+        public double convertir()
+        {
+            return ((this.getCentigrados() * 9 / 5) + 32);
+
+               // formula = (°C × 9 / 5) + 32
+        }
+        
+    }
+}
